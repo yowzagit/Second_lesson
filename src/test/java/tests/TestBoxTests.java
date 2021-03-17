f3361e719ee2937f41f2f19ae336d3867e8dbf4c
Aleksey <yowzamail@gmail.com>
2021-03-17 22:09:23 +0300

    1.Boost .gitignore 2.Add examples Junit5 3.New gradle with blackjack and sluts

---
+++ src/test/java/tests/TestBoxTests.java
@@ -0,0 +1,15 @@
+//package tests;
+//
+//import org.junit.jupiter.api.Test;
+//
+//import static com.codeborne.selenide.Selenide.open;
+//
+//public class TestBoxTests {
+//    @Test
+//    void successfulFillTest() {
+//        open("https://demoqa.com/test-box");
+//
+//        System.out.println("debug");
+//    }
+//
+//}
