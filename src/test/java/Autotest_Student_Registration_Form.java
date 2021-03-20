7d13c57c15147519673a8c0d0091d658d2c068bf
Aleksey <yowzamail@gmail.com>
2021-03-20 17:58:17 +0300

    fix several things

--- src/test/java/Autotest_Student_Registration_Form.java
+++ src/test/java/Autotest_Student_Registration_Form.java
@@ -9,12 +9,12 @@ public class Autotest_Student_Registration_Form {
 
 
     @BeforeAll
-    static void HiRes() {
+    static void setup() {
         Configuration.startMaximized = true;
     }
 
     @Test
-    void MiddleTest() {
+    void middleTest() {
 
         //присвоение имён
         String name = "Gregory";
@@ -22,8 +22,12 @@ public class Autotest_Student_Registration_Form {
         String email = "who_know_i_not@gmail.com";
         String sex = "Male";
         String number = "8005553535";
+        String birthMonth = "June";
+        String birthYear = "1966";
+        String subjects = "Maths";
+        String hobbies = "Reading";
 
-        String addr = "Budapeshtskaya street 98k3 kv.131 and kv.350, Saint-Petersburg, Russia";
+        String address = "Budapeshtskaya street 98k3 kv.131 and kv.350, Saint-Petersburg, Russia";
         String state = "NCR";
         String city = "Delhi";
 
@@ -37,23 +41,14 @@ public class Autotest_Student_Registration_Form {
         $("#genterWrapper").find(byText(sex)).click();
         $("#userNumber").setValue(number);
 
-        //today
-        //$("#dateOfBirthInput").pressEnter();
         $("#dateOfBirthInput").click();
-
-        //костыль
-        $(".react-datepicker__navigation.react-datepicker__navigation--next").click();
-        $(".react-datepicker__navigation.react-datepicker__navigation--next").click();
-        $(".react-datepicker__navigation.react-datepicker__navigation--next").click();
-        //sleep(222);
-        $(".react-datepicker__year-select").selectOptionByValue("1966");
-
-        //sleep(222);
+        $(".react-datepicker__month-select").selectOption(birthMonth);
+        $(".react-datepicker__year-select").selectOption(birthYear);
         $(".react-datepicker__day.react-datepicker__day--013").click();
 
+        $("#subjectsInput").setValue(subjects).pressEnter();
 
-
-        $("#currentAddress").setValue(addr);
+        $("#currentAddress").setValue(address);
 
         $("#state").click();
         $(byText(state)).click();
@@ -61,11 +56,12 @@ public class Autotest_Student_Registration_Form {
         $(byText(city)).click();
 
         $("#uploadPicture").uploadFromClasspath("per.png");
+        $("[for='hobbies-checkbox-2']").click();
 
         $("#submit").click();
 
         //проверка
-        $(".table").shouldHave(text(name), text(lastName), text(email), text(sex), text(number), text(addr), text("per.png"), text(state), text(city));
+        $(".table").shouldHave(text(name), text(lastName), text(email), text(sex), text(number), text(birthMonth), text(birthYear), text(subjects), text(hobbies), text(address), text("per.png"), text(state), text(city));
 
         $("#closeLargeModal").click();
 
